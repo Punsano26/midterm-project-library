1db549dd0d08b31d84d13d1a5c2999a743d8e7b3
Punsano26 <xlichaphlreuxn@gmail.com>
2024-09-18 00:16:40 +0700

    Create backend-login-reg&middlewareJWT

---
+++ Backend-library/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+require("dotenv").config(".env");
+const PORT = process.env.PORT || 2000;
+const authRouter = require("./routers/auth.router");
+const db = require("./models/index");
+const role = db.Role
+const cors = require("cors");
+
+const corsOption = {
+    origin: "http://localhost:5173",
+  };
+
+//   db.sequelize.sync({ force: true }).then(() => {
+//   initRole();
+//   console.log("Drop and re-sync db.");
+// });
+
+  const initRole = () => {
+    role.create({
+      id: 1,
+      name: "user",
+    });
+    role.create({
+      id: 2,
+      name: "moderator",
+    });
+    role.create({
+      id: 3,
+      name: "admin",
+    });
+  };
+
+//use middleware
+app.use(cors(corsOption));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+  //use Router
+  app.use("/api/v1/auth", authRouter);
+
+  app.get("/", (req, res) => {
+    res.send("<h1>Hello Librarys API</h1>");
+  });
+
+  app.listen(PORT, () => {
+    console.log("Listenning to http://localhost:" + PORT);
+  });
