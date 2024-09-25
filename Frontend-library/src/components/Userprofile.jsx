35a9e20d8fcad610da1e551346baae694fdbf8f0
Punsano26 <xlichaphlreuxn@gmail.com>
2024-09-26 00:49:02 +0700

    userprol=file

--- Frontend-library/src/components/Userprofile.jsx
+++ Frontend-library/src/components/Userprofile.jsx
@@ -30,29 +30,37 @@ const Userprofile = () => {
   };
   
   return (
-    <div>
-        <div className="dropdown dropdown-end">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+    <div className="flex items-center">
+    {/* Dropdown for User Profile */}
+    <div className="dropdown dropdown-end">
+      <div
+        tabIndex={0}
+        role="button"
+        className="btn btn-ghost btn-circle avatar"
+      >
         <div className="w-10 rounded-full">
           <img
-            alt="Tailwind CSS Navbar component"
-            src="https://pics.craiyon.com/2024-04-15/Ne_ExpINQpmIUGKzazfssA.webp" />
+            alt="Profile"
+            src="https://pics.craiyon.com/2024-04-15/Ne_ExpINQpmIUGKzazfssA.webp"
+          />
         </div>
       </div>
       <ul
         tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        className="menu menu-compact dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow md:w-64 lg:w-72"
+      >
         <li>
           <a href='/userprofile' className="justify-between">
             Profile
             <span className="badge">New</span>
           </a>
         </li>
-        <li><a>Settings</a></li>
+        <li><a href="#">Settings</a></li>
         <li><a onClick={handleLogout}>Logout</a></li>
       </ul>
     </div>
-    </div>
+  </div>
+  
   )
 }
 
