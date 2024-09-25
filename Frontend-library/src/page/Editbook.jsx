64d80fb11a75d8cafef6e1649ca3d565852cbedc
Punsano26 <xlichaphlreuxn@gmail.com>
2024-09-25 16:55:14 +0700

    deployer

--- Frontend-library/src/page/Editbook.jsx
+++ Frontend-library/src/page/Editbook.jsx
@@ -159,7 +159,7 @@ useEffect(() => {
           />
         </div>
 
-         {/* Price page */}
+         {/* Price */}
          <div className="mb-4">
           <label className="block text-sm font-medium mb-1">Price</label>
           <input
